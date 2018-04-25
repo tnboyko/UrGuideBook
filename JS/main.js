911adbaa3702b834cae1f18ad472684353fa8986
tnboyko <obolyab@gmail.com>
2018-04-25 18:36:40 +0300

    smaller icons for markers

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+var map;
 function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setLocation);
@@ -9,49 +10,19 @@ function setLocation(position){
     var userPositionLatitude = position.coords.latitude;
     var userPositionLongitude = position.coords.longitude;
     var coordinates = new google.maps.LatLng(userPositionLatitude, userPositionLongitude);
-    var map = new google.maps.Map(document.getElementById("map"),{
+    map = new google.maps.Map(document.getElementById("map"),{
         center: coordinates,
-         zoom: 14,
-         mapTypeId: 'roadmap'
+        zoom: 14,
+        mapTypeId: 'roadmap'
     });
-
     var markerMy = new google.maps.Marker({position:coordinates, map:map, title:"You are here!"});
-
-
-    var icons = {
-        cafe: {
-            icon: "UrGuideBook/icons/coffee-512.png",
-        },
-        pub: {
-            icon: 'UrGuideBook/icons/Beer-512.png',
-        },
-        pizza: {
-            icon: 'UrGuideBook/icons/map-icon.png'
-        }
-    };
-
-
-    var features = [
-        {
-            position: new google.maps.LatLng(50.5022695,30.4991657),
-            type: 'cafe'
-        }, {
-            position: new google.maps.LatLng(50.5016587,30.4953945),
-            type: 'pub'
-        }, {
-            position: new google.maps.LatLng(50.5061692,30.4975188),
-            type: 'pizza'
-        }
-    ]
-
-    features.forEach(function(feature) {
-        var marker = new google.maps.Marker({
-            position: feature.position,
-            icon: icons[feature.type].icon,
-            map: map
-        });
-});
 }
+
+
+
+
+
+
 
 
 
